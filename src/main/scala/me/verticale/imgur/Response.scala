2c7b8c9cf5d57096e85613f8456ef5277c130fac
Brice Thomas <brice.p.thomas@gmail.com>
2015-03-14 23:38:52 +0100

    :lipstick:

--- src/main/scala/me/verticale/imgur/Response.scala
+++ src/main/scala/me/verticale/imgur/Response.scala
@@ -2,8 +2,7 @@ package me.verticale.imgur
 
 import me.verticale.imgur.model._
 
-/** Responses API format
-  */
+/** Responses API format */
 case class ImageResponse(data: Image, success: Boolean, status: Int)
 case class ImagesResponse(data: List[Image], success: Boolean, status: Int)
 case class AlbumResponse(data: Album, success: Boolean, status: Int)
